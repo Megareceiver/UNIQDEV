f3058aef37f33ba70bed6142b9091f6041695fea
megan <megareceiver@gmail.com>
2017-08-24 22:56:21 +0700

    Update notification
    fixing bugs

--- js/router.js
+++ js/router.js
@@ -751,91 +751,91 @@ function r_fLogin() {
 	});
 }
 
-function r_fNotification() {
-	$("body").prepend(preload);
-	$('main.parent').animate({'opacity': '0.6'},'fast','linear', function(){
-		mainPage.html('');
-		head  	= '';
-		body  	= '';
-		part	= ['','','',''];
-		content = '';
-		data    = [
-			{
-				'group': 'Baru', 'list':[
-					{'noreg': '1', 'time': '2017/06/1 10:15', 'caption': 'lorem dolor sit amet 2.'},
-					{'noreg': '2', 'time': '2017/05/30 10:15', 'caption': 'lorem dolor sit amet 3.'},
-					{'noreg': '3', 'time': '2017/05/29 10:15', 'caption': 'lorem dolor sit amet 4.'},
-				]
-			},
-			{
-				'group': 'Riwayat', 'list':[
-					{'noreg': '4', 'time': '2017/05/11 10:15', 'caption': 'lorem dolor sit amet 6.'},
-					{'noreg': '5', 'time': '2017/05/14 10:15', 'caption': 'lorem dolor sit amet 7.'},
-					{'noreg': '6', 'time': '2017/05/15 10:15', 'caption': 'lorem dolor sit amet 8.'},
-					{'noreg': '7', 'time': '2017/05/20 10:15', 'caption': 'lorem dolor sit amet 9.'},
-				]
-			},
-		];
+// function r_fNotification() {
+// 	$("body").prepend(preload);
+// 	$('main.parent').animate({'opacity': '0.6'},'fast','linear', function(){
+// 		mainPage.html('');
+// 		head  	= '';
+// 		body  	= '';
+// 		part	= ['','','',''];
+// 		content = '';
+// 		data    = [
+// 			{
+// 				'group': 'Baru', 'list':[
+// 					{'noreg': '1', 'time': '2017/06/1 10:15', 'caption': 'lorem dolor sit amet 2.'},
+// 					{'noreg': '2', 'time': '2017/05/30 10:15', 'caption': 'lorem dolor sit amet 3.'},
+// 					{'noreg': '3', 'time': '2017/05/29 10:15', 'caption': 'lorem dolor sit amet 4.'},
+// 				]
+// 			},
+// 			{
+// 				'group': 'Riwayat', 'list':[
+// 					{'noreg': '4', 'time': '2017/05/11 10:15', 'caption': 'lorem dolor sit amet 6.'},
+// 					{'noreg': '5', 'time': '2017/05/14 10:15', 'caption': 'lorem dolor sit amet 7.'},
+// 					{'noreg': '6', 'time': '2017/05/15 10:15', 'caption': 'lorem dolor sit amet 8.'},
+// 					{'noreg': '7', 'time': '2017/05/20 10:15', 'caption': 'lorem dolor sit amet 9.'},
+// 				]
+// 			},
+// 		];
 
-		data = p_getData('fNotification', 'f110', '');
+// 		data = p_getData('fNotification', 'f110', '');
 		
-		//--open
-		head = '';
-		body = '<div class="row"><div class="container"><div class="col-md-8 col-md-offset-2">';
+// 		//--open
+// 		head = '';
+// 		body = '<div class="row"><div class="container"><div class="col-md-8 col-md-offset-2">';
 		
-		if(data != null){
-			for(var loop = 0; loop < data.length; loop++){
-				//--group
-				body = body + 
-				'<div class="cards-label plus">' +
-					'<p>' +
-						'<strong>' + data[loop].group + ' (' + data[loop].list.length + ')</strong>' +
-					'</p>' +
-				'</div>';
+// 		if(data != null){
+// 			for(var loop = 0; loop < data.length; loop++){
+// 				//--group
+// 				body = body + 
+// 				'<div class="cards-label plus">' +
+// 					'<p>' +
+// 						'<strong>' + data[loop].group + ' (' + data[loop].list.length + ')</strong>' +
+// 					'</p>' +
+// 				'</div>';
 				
-				for(var look = 0; look < data[loop].list.length; look++){
-					body = body + 
-					'<div class="cards bakcup-list">' +
-						'<div class="row default">' +
-							'<div class="col-xs-10">' +
-								'<div class="list-box">' +
-									'<div class="list-icon bg-yellow"><span class="fa fa-bell"></span></div>' +
-									'<p class="list-text">' + data[loop].list[look].caption + '</p>' +
-								'</div>' +
-							'</div>' +
-							'<div class="col-xs-2">' +
-								'<div class="list-box clear">' +
-									'<p class="list-text">' + timeSince(new Date(Date.parse(data[loop].list[look].time))) + '</p>' +
-								'</div>' +
-							'</div>' +
-							'<div class="clearfix"></div>' +
-						'</div>' +
-					'</div>';
-				}
-			}	
-		}else{
-			body = body +
-			'<div class="cards">' +
-				'<div class="cards-header">' +
-					'<p class="fixed offset text-black">Tidak ada pemberitahuan.</p>' +
-				'</div>' +
-			'</div>';
-		}	
+// 				for(var look = 0; look < data[loop].list.length; look++){
+// 					body = body + 
+// 					'<div class="cards bakcup-list">' +
+// 						'<div class="row default">' +
+// 							'<div class="col-xs-10">' +
+// 								'<div class="list-box">' +
+// 									'<div class="list-icon bg-yellow"><span class="fa fa-bell"></span></div>' +
+// 									'<p class="list-text">' + data[loop].list[look].caption + '</p>' +
+// 								'</div>' +
+// 							'</div>' +
+// 							'<div class="col-xs-2">' +
+// 								'<div class="list-box clear">' +
+// 									'<p class="list-text">' + timeSince(new Date(Date.parse(data[loop].list[look].time))) + '</p>' +
+// 								'</div>' +
+// 							'</div>' +
+// 							'<div class="clearfix"></div>' +
+// 						'</div>' +
+// 					'</div>';
+// 				}
+// 			}	
+// 		}else{
+// 			body = body +
+// 			'<div class="cards">' +
+// 				'<div class="cards-header">' +
+// 					'<p class="fixed offset text-black">Tidak ada pemberitahuan.</p>' +
+// 				'</div>' +
+// 			'</div>';
+// 		}	
 		
-		body	= body + '</div></div></div>';
-		content = '<section id="">' + head + body + '</section>';
-		//--close
+// 		body	= body + '</div></div></div>';
+// 		content = '<section id="">' + head + body + '</section>';
+// 		//--close
 		
-		//--gen
-		headPage.html(r_headPageHtml(3, 'Pemberitahuan'));
-		mainPage.html(content).animate({'opacity': '1'},'fast','linear');
-		$("#preload").remove();
+// 		//--gen
+// 		headPage.html(r_headPageHtml(3, 'Pemberitahuan'));
+// 		mainPage.html(content).animate({'opacity': '1'},'fast','linear');
+// 		$("#preload").remove();
 		
-		//--command reactor
-		$(".back-button").unbind().on('click', function(){ r_navigateTo(r_pagePreviousReader()); });
-		r_navbarReactor();
-	});
-}
+// 		//--command reactor
+// 		$(".back-button").unbind().on('click', function(){ r_navigateTo(r_pagePreviousReader()); });
+// 		r_navbarReactor();
+// 	});
+// }
 
 /*F0*/
 function r_f0Dashboard() {
--- js/subRouter.js
+++ js/subRouter.js
@@ -8,59 +8,49 @@ function r_fNotification() {
 		part	= ['','','',''];
 		content = '';
 		data	= p_getData('fNotification', 'f110', '');
-		// data2 = 'adaan';
-		console.log(data); 
-		// data    = [
-		// 	{
-		// 		'group': 'Baru', 'list':[
-		// 			{'noreg': '1', 'time': '2017/06/1 10:15', 'caption': 'lorem dolor sit amet 2.'},
-		// 			{'noreg': '2', 'time': '2017/05/30 10:15', 'caption': 'lorem dolor sit amet 3.'},
-		// 			{'noreg': '3', 'time': '2017/05/29 10:15', 'caption': 'lorem dolor sit amet 4.'},
-		// 		]
-		// 	},
-		// 	{
-		// 		'group': 'Riwayat', 'list':[
-		// 			{'noreg': '4', 'time': '2017/05/11 10:15', 'caption': 'lorem dolor sit amet 6.'},
-		// 			{'noreg': '5', 'time': '2017/05/14 10:15', 'caption': 'lorem dolor sit amet 7.'},
-		// 			{'noreg': '6', 'time': '2017/05/15 10:15', 'caption': 'lorem dolor sit amet 8.'},
-		// 			{'noreg': '7', 'time': '2017/05/20 10:15', 'caption': 'lorem dolor sit amet 9.'},
-		// 		]
-		// 	},
-		// ];
 		
 		//--open
 		head = '';
-		body = '<div class="row"><div class="container"><div class="col-md-8 col-md-offset-2">';
+		body = '<div class="row no-head"><div class="container"><div class="col-md-8 col-md-offset-2">';
 		
-		for(var loop = 0; loop < data.feedData.length; loop++){
-			//--group
-			body = body + 
-			'<div class="cards-label plus">' +
-				'<p>' +
-					'<strong>' + data.feedData[loop].group + ' (' + data.feedData[loop].list.length + ')</strong>' +
-				'</p>' +
-			'</div>';
-			
-			for(var look = 0; look < data.feedData[loop].list.length; look++){
+		if(data.feedData != null){
+			for(var loop = 0; loop < data.feedData.length; loop++){
+				//--group
 				body = body + 
-				'<div class="cards bakcup-list">' +
-					'<div class="row default">' +
-						'<div class="col-xs-10">' +
-							'<div class="list-box">' +
-								'<div class="list-icon bg-yellow"><span class="fa fa-bell"></span></div>' +
-								'<p class="list-text">' + data.feedData[loop].list[look].deskripsi + '</p>' +
-							'</div>' +
-						'</div>' +
-						'<div class="col-xs-2">' +
-							'<div class="list-box clear">' +
-								'<p class="list-text">' + timeSince(new Date(Date.parse(data.feedData[loop].list[look].waktu))) + '</p>' +
-							'</div>' +
-						'</div>' +
-						'<div class="clearfix"></div>' +
-					'</div>' +
+				'<div class="cards-label plus">' +
+					'<p>' +
+						'<strong>' + data.feedData[loop].group + ' (' + data.feedData[loop].list.length + ')</strong>' +
+					'</p>' +
 				'</div>';
-			}
-		}	
+				
+				for(var look = 0; look < data.feedData[loop].list.length; look++){
+					body = body + 
+					'<div class="cards bakcup-list">' +
+						'<div class="row default">' +
+							'<div class="col-xs-10">' +
+								'<div class="list-box">' +
+									'<div class="list-icon bg-yellow"><span class="fa fa-bell"></span></div>' +
+									'<p class="list-text">' + data.feedData[loop].list[look].deskripsi + '</p>' +
+								'</div>' +
+							'</div>' +
+							'<div class="col-xs-2">' +
+								'<div class="list-box clear">' +
+									'<p class="list-text">' + timeSince(new Date(Date.parse(data.feedData[loop].list[look].waktu))) + '</p>' +
+								'</div>' +
+							'</div>' +
+							'<div class="clearfix"></div>' +
+						'</div>' +
+					'</div>';
+				}
+			}	
+		}else{
+			body = body +
+			'<div class="cards">' +
+				'<div class="cards-header">' +
+					'<p class="fixed offset text-black">Tidak ada pemberitahuan.</p>' +
+				'</div>' +
+			'</div>';
+		}
 		
 		body	= body + '</div></div></div>';
 		content = '<section id="">' + head + body + '</section>';
